789c4778092be7c9c4b2027f8347c7e498b264c8
Marc Bäckman <93243973+mabackma@users.noreply.github.com>
2024-04-14 03:57:18 +0300

    changed port to 8080

--- app.py
+++ app.py
@@ -80,4 +80,4 @@ def hello_world():
     return {'sentiment': prediction}
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=8080)
